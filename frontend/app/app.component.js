5dfb5751f4c99e7dd903236dc1884136abecf86a
thepercival <git30903>
2016-11-09 22:11:13 +0100

    seperated frontend and backend

--- frontend/app/app.component.js
+++ frontend/app/app.component.js
@@ -9,13 +9,59 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var CompetitionSeason = (function () {
+    function CompetitionSeason() {
+    }
+    return CompetitionSeason;
+}());
+exports.CompetitionSeason = CompetitionSeason;
+var COMPETITIONSEASONS = [
+    { id: 11, name: 'EK', seasonname: '2012/2013' },
+    { id: 12, name: 'WK', seasonname: '2013/2014' },
+    { id: 13, name: 'WK', seasonname: '2012/2013' }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'FCToernooi';
+        this.competitionseasons = COMPETITIONSEASONS;
+        this.competitionseason = { 'id': 1, 'name': 'wk 2018' };
     }
+    AppComponent.prototype.onSelect = function (competitionseason) {
+        this.selectedCompetitionSeason = competitionseason;
+    };
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: '<h1>FCToernooi</h1>'
+            selector: 'fctoernooi-app',
+            template: '<h1>{{title}}</h1>' +
+                '<hr/>' +
+                '<table class="u-full-width">' +
+                '<thead>' +
+                '<tr>' +
+                '<th>Naam</th>' +
+                '<th>Seizoen</th>' +
+                '</tr>' +
+                '</thead>' +
+                '<tbody>' +
+                '<tr *ngFor="let competitionseason of competitionseasons" class="competitionseason" (click)="onSelect(competitionseason)">' +
+                '<td>{{competitionseason.name}}</td><td>{{competitionseason.seasonname}}</td>' +
+                '</tr>' +
+                '</tbody>' +
+                '</table>' +
+                '<h2>toernooi</h2>' +
+                '' +
+                '<div *ngIf="selectedCompetitionSeason">' +
+                '<div><label>id: </label>{{selectedCompetitionSeason.id}}</div>' +
+                '<div>' +
+                '<label>naam: </label>' +
+                '<input type="text" class="u-full-width" [(ngModel)]="selectedCompetitionSeason.name" placeholder="naam">' +
+                '</div>' +
+                '<div>' +
+                '<label>naam: </label>' +
+                '<input type="text" class="u-full-width" [(ngModel)]="selectedCompetitionSeason.seasonname" placeholder="2012/2013">' +
+                '</div>' +
+                '</div>' +
+                '',
+            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;    \n  }\n  .competitionseasons {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  tr.competitionseason {\n    cursor: pointer;\n  }\n  .competitionseasons li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .competitionseasons li.selected:hover {\n    background-color: #BBD8DC !important;    \n  }\n  .competitionseasons li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .competitionseasons .text {\n    position: relative;\n    top: -3px;\n  }\n  .competitionseasons .badge {\n    display: inline-block;\n    font-size: small;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,6 +1,105 @@
 import { Component } from '@angular/core';
+
+export class CompetitionSeason {
+    id: number;
+    name: string;
+}
+
+const COMPETITIONSEASONS: CompetitionSeason[] = [
+    { id: 11, name: 'EK', seasonname: '2012/2013' },
+    { id: 12, name: 'WK', seasonname: '2013/2014' },
+    { id: 13, name: 'WK', seasonname: '2012/2013' }
+];
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>FCToernooi</h1>'
+    selector: 'fctoernooi-app',
+    template: '<h1>{{title}}</h1>'+
+    '<hr/>'+
+    '<table class="u-full-width">'+
+    '<thead>'+
+    '<tr>'+
+    '<th>Naam</th>'+
+    '<th>Seizoen</th>'+
+    '</tr>'+
+    '</thead>'+
+    '<tbody>'+
+    '<tr *ngFor="let competitionseason of competitionseasons" class="competitionseason" (click)="onSelect(competitionseason)">'+
+    '<td>{{competitionseason.name}}</td><td>{{competitionseason.seasonname}}</td>'+
+    '</tr>'+
+    '</tbody>'+
+    '</table>'+
+    '<h2>toernooi</h2>' +
+    ''+
+    '<div *ngIf="selectedCompetitionSeason">'+
+    '<div><label>id: </label>{{selectedCompetitionSeason.id}}</div>'+
+    '<div>'+
+    '<label>naam: </label>'+
+    '<input type="text" class="u-full-width" [(ngModel)]="selectedCompetitionSeason.name" placeholder="naam">'+
+    '</div>'+
+    '<div>'+
+    '<label>naam: </label>'+
+    '<input type="text" class="u-full-width" [(ngModel)]="selectedCompetitionSeason.seasonname" placeholder="2012/2013">'+
+    '</div>'+
+    '</div>' +
+    '',
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;    
+  }
+  .competitionseasons {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  tr.competitionseason {
+    cursor: pointer;
+  }
+  .competitionseasons li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .competitionseasons li.selected:hover {
+    background-color: #BBD8DC !important;    
+  }
+  .competitionseasons li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .competitionseasons .text {
+    position: relative;
+    top: -3px;
+  }
+  .competitionseasons .badge {
+    display: inline-block;
+    font-size: small;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'FCToernooi';
+    competitionseasons = COMPETITIONSEASONS;
+    competitionseason: CompetitionSeason = { 'id' : 1, 'name' : 'wk 2018' };
+    selectedCompetitionSeason: CompetitionSeason;
+
+    onSelect(competitionseason: CompetitionSeason): void {
+        this.selectedCompetitionSeason = competitionseason;
+    }
+}
