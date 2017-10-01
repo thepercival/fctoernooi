236e1d512efc0c9644301ca0a2f010bd2f134485
thepercival <git30903>
2017-10-01 21:42:05 +0200

    added screen for new tournament

---
+++ backend/domain/Tournament/Service.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coen
+ * Date: 1-10-17
+ * Time: 21:41
+ */
+
+/*
+ * this service should be a wrapper to create a competitionseason and competitionseasonroles!!!
+ */
