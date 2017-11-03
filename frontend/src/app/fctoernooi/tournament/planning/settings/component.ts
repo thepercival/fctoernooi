7af7455ee74b7ded0aa41f4c24dce2678a8e69dc
thepercival <coendunnink@gmail.com>
2017-11-04 00:13:40 +0100

    added config round

--- frontend/src/app/fctoernooi/tournament/planning/settings/component.ts
+++ frontend/src/app/fctoernooi/tournament/planning/settings/component.ts
@@ -3,6 +3,7 @@ import { Tournament } from '../../../tournament';
 import { Round } from 'voetbaljs/round';
 import { StructureService } from 'voetbaljs/structure/service';
 import { RoundConfig } from 'voetbaljs/round/config';
+import { RoundScoreConfig } from 'voetbaljs/round/scoreconfig';
 import { PlanningService } from 'voetbaljs/planning/service';
 
 @Component({
@@ -16,25 +17,30 @@ export class TournamentPlanningSettingsComponent implements OnInit {
     @Input() round: Round;
     @Input() structureService: StructureService;
 
+    enableTime: boolean;
     ranges: any = {};
     config: RoundConfig;
     private planningService: PlanningService;
-    validations: any = {
+   validations: any = {
         'minNrOfHeadtoheadMatches' : 1,
         'maxNrOfHeadtoheadMatches' : 4,
         'minWinPoints' : 1,
         'maxWinPoints' : 10,
         'minDrawPoints' : 1,
         'maxDrawPoints' : 5,
-        'minNrOfMinutesPerGame' : 1,
-        'maxNrOfMinutesPerGame' : 30,
+        'minMinutesPerGame' : 0,
+        'maxMinutesPerGame' : 60,
     };
+    // this.complexForm = fb.group({
+    //     'minutesPerGame': [null, maxValue(60)]
+    // })
 
     constructor() {
     }
 
     ngOnInit() {
         this.config = this.round.getConfig();
+        this.enableTime = this.config.getMinutesPerGame() > 0;
         console.log(this.config);
 
         this.initRanges();
@@ -86,11 +92,100 @@ export class TournamentPlanningSettingsComponent implements OnInit {
         this.config.setWinPoints( winPoints );
     }
 
+
     setDrawPoints( drawPoints ) {
         if ( drawPoints < this.validations.minDrawPoints
             || drawPoints > this.validations.maxDrawPoints ) {
             return;
         }
         this.config.setDrawPoints( drawPoints );
+    }
+
+    setHasExtension( hasExtension ) {
+        if ( hasExtension !== true && hasExtension !== false ) {
+            return;
+        }
+        this.config.setHasExtension( hasExtension );
+        this.planningService.reschedule( this.round );
+    }
+
+    setWinPointsExt( winPointsExt ) {
+        if ( winPointsExt < this.validations.minWinPoints
+            || winPointsExt > this.validations.maxWinPoints ) {
+            return;
+        }
+        this.config.setWinPointsExt( winPointsExt );
+    }
+
+    setDrawPointsExt( drawPointsExt ) {
+        if ( drawPointsExt < this.validations.minDrawPoints
+            || drawPointsExt > this.validations.maxDrawPoints ) {
+            return;
+        }
+        this.config.setDrawPointsExt( drawPointsExt );
+    }
+
+    setMinutesPerGameExt( minutesPerGameExt ) {
+        if ( minutesPerGameExt < this.validations.minMinutesPerGame
+            || minutesPerGameExt > this.validations.maxMinutesPerGame ) {
+            const avg = Math.floor( ( this.validations.minMinutesPerGameExt + this.validations.maxMinutesPerGameExt ) / 2 );
+            this.config.setMinutesPerGameExt( avg );
+            return;
+        }
+        this.config.setMinutesPerGameExt( minutesPerGameExt );
+        if ( this.config.getHasExtension() && this.config.getEnableTime() ) {
+            this.planningService.reschedule(this.round);
+        }
+    }
+
+    setMinutesPerGame( minutesPerGame ) {
+        console.log(minutesPerGame);
+        if ( minutesPerGame < this.validations.minMinutesPerGame
+            || minutesPerGame > this.validations.maxMinutesPerGame ) {
+            console.log('wrong value', minutesPerGame);
+            const avg = Math.floor( ( this.validations.minMinutesPerGame + this.validations.maxMinutesPerGame ) / 2 );
+            console.log('new value', avg);
+            this.config.setMinutesPerGame( avg );
+            return;
+        }
+        this.config.setMinutesPerGame( minutesPerGame );
+        if ( this.config.getEnableTime() ) {
+            this.planningService.reschedule(this.round);
+        }
+    }
+
+    setMinutesInBetween( minutesInBetween ) {
+        if ( minutesInBetween < this.validations.minMinutesPerGame
+            || minutesInBetween > this.validations.maxMinutesPerGame ) {
+            const avg = Math.floor( this.config.getMinutesPerGame() / 2 );
+            this.config.setMinutesInBetween( avg );
+            return;
+        }
+        this.config.setMinutesInBetween( minutesInBetween );
+        if ( this.config.getEnableTime() ) {
+            this.planningService.reschedule(this.round);
+        }
+    }
+
+    setEnableTime( enableTime ) {
+        if ( enableTime !== true && enableTime !== false ) {
+            return;
+        }
+        this.config.setEnableTime( enableTime );
+        this.planningService.reschedule( this.round );
+    }
+
+    setScoreConfigStart( scoreConfig: RoundScoreConfig, scoreConfigStart ) {
+        if ( scoreConfigStart > 9999 || scoreConfigStart < 0 ) {
+            return;
+        }
+        scoreConfig.setStart( scoreConfigStart );
+    }
+
+    setScoreConfigGoal( scoreConfig: RoundScoreConfig, scoreConfigGoal ) {
+        if ( scoreConfigGoal > 9999 || scoreConfigGoal < 0 ) {
+            return;
+        }
+        scoreConfig.setGoal( scoreConfigGoal );
     }
 }
