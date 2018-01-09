9de1fd1dfbd0b8f95e2cfe176b1f366e775a3cbe
thepercival <coendunnink@gmail.com>
2018-01-09 20:05:40 +0100

    fixed ranking

--- frontend/src/app/fctoernooi/tournament/planning/view/component.ts
+++ frontend/src/app/fctoernooi/tournament/planning/view/component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Router } from '@angular/router';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap/popover/popover';
 import { Game } from 'voetbaljs/game';
 import { PlanningService } from 'voetbaljs/planning/service';
 import { Poule } from 'voetbaljs/poule';
 import { PoulePlace } from 'voetbaljs/pouleplace';
+import { Ranking } from 'voetbaljs/ranking';
 import { Round } from 'voetbaljs/round';
 import { StructureService } from 'voetbaljs/structure/service';
 
@@ -24,11 +26,14 @@ export class TournamentPlanningViewComponent implements OnInit {
   planningService: PlanningService;
   GameStatePlayed = Game.STATE_PLAYED;
   selectedPouleForRanking;
+  private openPopovers: NgbPopover[] = [];
+  ranking: Ranking;
   // public winnersAndLosers: number[];
 
   constructor(private router: Router) {
     // this.winnersAndLosers = [Round.WINNERS, Round.LOSERS];
     this.resetAlert();
+    this.ranking = new Ranking(Ranking.RULESSET_WC);
   }
 
   ngOnInit() {
@@ -75,14 +80,28 @@ export class TournamentPlanningViewComponent implements OnInit {
     );
   }
 
-  showRanking(poule: Poule) {
+  showRanking(popOver: NgbPopover, poule: Poule) {
+    this.openPopovers.forEach(openPopover => openPopover.close());
+    const open = (!this.openPopovers.find(openPopover => openPopover === popOver));
+    this.openPopovers = [];
     this.selectedPouleForRanking = poule;
+    if (open === true) {
+      popOver.open();
+      this.openPopovers.push(popOver);
+    }
     return false;
+  }
+
+  getPoulePlacesByRank(poule: Poule): PoulePlace[][] {
+    const pp = this.ranking.getPoulePlacesByRank(poule.getPlaces(), poule.getGames());
+    console.log(pp);
+    return pp;
   }
 
   protected resetAlert(): void {
     this.alert = undefined;
   }
+
 
   protected setAlert(type: string, message: string): boolean {
     this.alert = { 'type': type, 'message': message };
