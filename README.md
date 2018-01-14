3241e7835bfe9eabd4bc6a8aece5fb3236572f6a
thepercival <coendunnink@gmail.com>
2018-01-14 12:30:35 +0100

    removed 51

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fctoernooi51
+# Fctoernooi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.3.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('fctoernooi51 App', () => {
+describe('fctoernooi App', () => {
   let page: AppPage;
 
   beforeEach(() => {
