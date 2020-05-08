1cf719eaf52ea2e7aa3d4d884c1788010085c52e
thepercival <coendunnink@gmail.com>
2020-05-08 16:22:20 +0200

    update api

--- src/app/lib/repository.ts
+++ src/app/lib/repository.ts
@@ -16,7 +16,7 @@ export class APIRepository {
 
     getHeaders(): HttpHeaders {
         let headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
-        headers = headers.append('X-Api-Version', '19');
+        headers = headers.append('X-Api-Version', '20');
 
         const token = this.getToken();
         if (token !== undefined) {
