fc8b9b95ac4c3b55c0330c87764c2d99df747ae5
thepercival <coendunnink@gmail.com>
2021-04-02 10:31:22 +0200

    remove bugs

--- src/app/lib/ngx-sport/defaultService.ts
+++ src/app/lib/ngx-sport/defaultService.ts
@@ -59,7 +59,7 @@ export class DefaultService {
      * 
      * @param sports 
      */
-    protected getDefaultGameCreationStrategy(sports: Sport[]): CreationStrategy {
+    protected getGameCreationStrategy(sports: Sport[]): CreationStrategy {
         // muliply
         if (sports.length > 1) {
             return CreationStrategy.StaticManual;
@@ -78,5 +78,64 @@ export class DefaultService {
 
     protected hasGameMode(sports: Sport[], gameMode: GameMode): boolean {
         return sports.some((sport: Sport) => sport.getGameMode() === gameMode);
+    }
+
+    getNrOfPoules(nrOfPlaces: number): number {
+        switch (nrOfPlaces) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 7: {
+                return 1;
+            }
+            case 6:
+            case 8:
+            case 10:
+            case 11: {
+                return 2;
+            }
+            case 9:
+            case 12:
+            case 13:
+            case 14:
+            case 15: {
+                return 3;
+            }
+            case 16:
+            case 17:
+            case 18:
+            case 19: {
+                return 4;
+            }
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 25: {
+                return 5;
+            }
+            case 24:
+            case 26:
+            case 29:
+            case 30:
+            case 33:
+            case 34:
+            case 36: {
+                return 6;
+            }
+            case 28:
+            case 31:
+            case 35:
+            case 37:
+            case 38:
+            case 39: {
+                return 7;
+            }
+            case 27: {
+                return 9;
+            }
+        }
+        return 8;
     }
 }
